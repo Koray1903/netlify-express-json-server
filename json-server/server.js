85d8365842bb6c4b38065e7afc44b03abf1c713e
Koray <59251087+Koray1903@users.noreply.github.com>
2020-11-20 15:30:36 +0200

    Update server.js

--- json-server/server.js
+++ json-server/server.js
@@ -12,7 +12,7 @@ app.use('/api', jsonServer.router(path.resolve(__dirname, 'db.json')));
 const router = express.Router();
 router.get('/', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello from Express.js!</h1>');
+    res.json('./db.json');
     res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
